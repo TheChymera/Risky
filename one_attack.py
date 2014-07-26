ffaf77b4cd3ea6560441488e647339370a164812
Horea Christian <h.chr@mail.ru>
2014-07-26 04:08:19 +0200

    initial commit

---
+++ one_attack.py
@@ -0,0 +1,55 @@
+from __future__ import division
+__author__ = 'Horea Christian'
+
+import numpy as np
+from itertools import product
+
+def odds_formulae(attackers, defenders, dice_sides=6, victory=0, tie=0, defeat=0):
+    if attackers == 1 and defenders == 2:
+	for n in np.arange(dice_sides)+1:
+	    victory += (n-1)*(dice_sides**attackers-n**attackers)/dice_sides**(attackers+2)
+	    for m in np.arange(dice_sides-n+1)+n:
+		victory += (dice_sides**attackers-m**attackers)/dice_sides**(attackers+2)
+	defeat = 1 - victory
+    elif attackers == 2 and defenders == 2:
+	for (n,m) in product(np.arange(dice_sides)+1, np.arange(dice_sides)+1):
+		victory += (dice_sides-n)*(dice_sides-m)/dice_sides**4
+	defeat = 1 - victory
+    elif defenders == 1:
+	for n in np.arange(dice_sides)+1:
+	    victory += (dice_sides**attackers-n**attackers)/dice_sides**(attackers+1)
+	defeat = 1 - victory
+    else:
+	print "Automatically switching to lookup-method to determine odds as your specified attacker and defender values have no corresponding odds formula."
+	victory, tie, defeat = odds_lookup(attackers, defenders, dice_sides, victory, tie, defeat)
+    
+    return victory, tie, defeat
+    
+def odds_lookup(attackers, defenders, dice_sides=6, victory=0, tie=0, defeat=0):
+    # The function has only been tested for attackers in (1,2,3) and defenders in (1,2).
+    
+    from itertools import product
+    fighting_units = min(attackers,defenders)
+    for sequence in np.array(list(product(np.arange(dice_sides)+1, repeat=(attackers+defenders)))):
+	#sort values ascendingly (for attackers) and descendingly (for defenders)
+	sequence = np.concatenate((np.sort(sequence[:attackers])[::-1],np.sort(sequence[-defenders:])))
+	score = 0
+	for a in np.arange(fighting_units):
+	    if sequence[a] > sequence[-(a+1)]:
+		score += 1
+	if score >= fighting_units:
+	    victory += 1
+	elif score == 0:
+	    tie += 1
+	else:
+	    defeat += 1
+	    	    
+    victory = victory/dice_sides**(attackers+defenders)
+    defeat = defeat/dice_sides**(attackers+defenders)
+    tie = tie/dice_sides**(attackers+defenders)
+    
+    if victory+tie+defeat != 1.0:
+	raise ValueError("The sum of probabilities does not equal 1. Please ceheck your input and that the function supports it.")
+    
+    return victory, tie, defeat
+
---
+++ risky_cli.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python
+__author__ = 'Horea Christian'
+import argparse
+import one_attack
+
+parser = argparse.ArgumentParser()
+parser.add_argument("attackers", help="number of attacking units (integer 1 to 3)", type=int)
+parser.add_argument("defenders", help="number of defending units (integer 1 to 2)", type=int)
+parser.add_argument("-f", "--force-lookup", help="force usage of the odds_lookup (non-formulaic, slow) method", action="store_true")
+parser.add_argument("-d", "--dice-sides", help="specify the number of faces on the dice", default=6, type=int)
+args = parser.parse_args()
+
+if str(args.dice_sides)[0] == 8:
+    n = "n"
+elif args.dice_sides == 11:
+    n = "n"
+else:
+    n = ""
+
+if args.attackers >= 2:
+    attackers_s="s"
+else:
+    attackers_s=""
+if args.defenders >= 2:
+    defenders_s="s"
+else:
+    defenders_s=""
+
+print"Using a"+n+" "+str(args.dice_sides)+"-sided dice and "+str(args.attackers)+" attacker"+attackers_s+" and "+str(args.defenders)+" defender"+defenders_s+", your outcome odds are:"
+
+if args.force_lookup:
+    victory, tie, defeat = one_attack.odds_lookup(args.attackers, args.defenders, dice_sides=args.dice_sides)
+    print "Victory =",victory
+    print "    Tie =",tie
+    print " Defeat =",defeat
+
+else:
+    victory, tie, defeat = one_attack.odds_formulae(args.attackers, args.defenders, dice_sides=args.dice_sides)
+    print "Victory =",victory
+    print "    Tie =",tie
+    print " Defeat =",defeat
